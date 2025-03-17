b7332e528dc283cda6fb34cba8fa5cfc50c54504
Lynco <s2202108@usls.edu.ph>
2025-03-17 08:56:22 +0800

    Created separate CSS file for Test App

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,28 +1,47 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>Test Data</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('test.css') }}">
 </head>
 <body>
-    <div class="container">
-        <h1>Test Data</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                    </tr>
-            </thead>
-            <tbody>
-                @foreach ($items as $item)
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">Test App</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="content">
+        <div class="centered-container">
+            <h1>Test Data</h1>
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $item->name }}</td>
-                        <td>${{ $item->price }}</td>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($items as $item)
+                        <tr>
+                            <td>{{ $item->name }}</td>
+                            <td>${{ $item->price }}</td>
                         </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
